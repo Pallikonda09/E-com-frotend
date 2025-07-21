751f484fa9b9a68b9f593f031dc38ff9a29fa498
Pallikonda09 <you@example.com>
2025-07-21 18:12:27 +0530

    Fix Netlify deployment config

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-base: './', 
+base: '/E-com-frotend/', // 
 
   server: {
     proxy: {
